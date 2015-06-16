2782f93bed6dd78fb184a1290ca0caa4cf7c5cb4
muzixinly <muzixinly@gmail.com>
2015-06-16 22:36:05 +0800

    add accounts.txt

--- tool/wx_reader.py
+++ tool/wx_reader.py
@@ -17,30 +17,8 @@ import random
 import traceback
 
 #待爬取的urls
-acccount_urls=[
 
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFty72GGlJl1Fa32fnPqybPV8&eqs=eJs2o1NgdWgsoZFJRezGQul0hrTWQVdFdCB4Dsagqyz2T%2Bz6FSUNa8zK97qTiqrdf%2Fm4i&ekv=4&page=1&t=',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt98u7kmyb9-OpSPghHa7Uiw&eqs=dfsroPXglYvyo2HGAmjLWuVv4Q2jJKpbccC1%2F4i6fPU0ImXKrPYUak6P0VLasvUp%2B%2FJXp&ekv=4&page=1&t=',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFtwpx4WaL2AzuAe1OmSHfB5Q&eqs=SdsSoOCgsGKho5duSruI%2BuDRyVQh1z46KW7eQDkfBpWyNlR4gV11X1PEMi78ZojF7a13A&ekv=4&page=1&t=1434250695417',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt86NKeSGd_BQKp1GcDkYpv0&eqs=A%2BsyoLcgkR8RouvjwSXfcusKGIjEjna4bHimBR%2FkiVzeX%2BIZUhMU6oD5ZRDWeBH7ec2Jp&ekv=4&page=1&t=1434250772825',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt9CaL2pJRKejPFmSWPpIroI&eqs=2AsFoUvgutnfoeE7UybItuEB%2Be2sLRBoHrrLeHvPXuynWpj0he4QxAie72ZUo38Z1mG49&ekv=4&page=1&t=1434308584971',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFtyYfYuoI1iJmzZ_zh3rwVA0&eqs=s4slo1jgXhoMoL1QNFlWIubtBHo7LGr0xkAoIvN1trS8uNJFmmPWMGZomBS7cbsss7HU1&ekv=4&page=1&t=1434308643334',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt455ps1TgT74uilbTe7-2cI&eqs=GTsEodzgtqyRou1Ua9so8udor56%2FrPI7R%2FcphhkYNFBJ2aaC0wo4ThoFg5lO2ZKoUa3w2&ekv=4&page=1&t=1434308679144',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt9OIEbzwI6cyUmBfCgBRHBU&eqs=dXsGov7g6H1XoN%2Bdbmx6UuMkve3%2FMqo4sP2m7VUmNT5N5gbx5N2cq5C4tot4XWUdj8EoA&ekv=4&page=1&t=1434351475413',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFtzcbFoqS4ip7ZkUQlz88kXQ&eqs=EZs1o1ngE2z0o53yaeOklu0XJ9wW%2BL66lMSdp1wO0esmQPqUA%2BEaQp8tK6vslVFCkV15F&ekv=4&page=1&t=1434351529698',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt7e7hq42aBOOeOQnFcLvKEM&eqs=OXsDoAcgf%2FAloJczvYP9au2q7CZ4J5L6ixtog93hF4iMQKce3HY6raMEQd62W46N7Js67&ekv=4&page=1&t=1434351591415',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFtz-ov6wZChK8smIQb4EXj_o&eqs=vPsuoH0gCrwVowtGmpyGkujMm7gjyp%2BfAK1pIUfOljfgRYJz0LxZSNFVIHUAu4fU8PjrH&ekv=4&page=1&t=1434351637880',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt1A9eVKH7qBQh5pf5tt5n-4&eqs=5GsMo9AgpwbWo1e%2FhnvA9ujIkUzPsGnswds0d8jbzBWNbVMtmWzFv9LlW1Rlsmw8WyXmr&ekv=4&page=1&t=1434351667848',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFtwHcjPKR_nOreR0yWpEYCh0&eqs=lssAoZAgAH67ofSTe6zQbuWeEbk6NanMJz6e0JmXNRSxAb5e8W3f6A%2BpYVeQbzEsgbi2W&ekv=4&page=1&t=1434351713765',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt-F95xcnWlUw2HLx0dqmArM&eqs=%2BwsaoBNgpR2jo4mChD8CmuLTXztM2is7%2F5lzk3pAXsQXWcM%2F%2BpZye9K56vkMDxQy2tVo3&ekv=4&page=1&t=1434351758168',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt7EVnIjsiOajYQ-jfO9L6QA&eqs=ycs%2BojwgDOUMo4KJ%2BIDxHuwDkVeHsKlm8kJaN2NjksUtyx5OZrtTC1r7aTH2H2eJ3GW67&ekv=4&page=1&t=1434351802511',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFtxG-2J2sGx3l5-pknZDv60g&eqs=68s3o0cgqq9nos3vVQDa7uG7MRxTH22GjsqXCOHytbPvUatX4mv%2F7nY2micOsFO6UoOh3&ekv=4&page=1&t=1434351835046',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt5Bg_S5FCRWYXZl4N_xG9hk&eqs=Z7sXoAKgVObjoz8yJ71nVuUuGyfoWv07os977qfkH0apBfNb%2FhBD%2B6pz9%2BZLQmIQwoMcX&ekv=4&page=1&t=1434351891117',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFtw5HO0UEXqv_gFvTWjE8rl0&eqs=C0sWoRTgCYsGodYSiHuTSudiApMsqlOE7WoxgnNjeh5jGuMZeID0xi%2BjqeZ07XK7fuZX3&ekv=4&page=1&t=1434351919865',
-'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=oIWsFt4bY1Ee_9GHdrk2pOgzczyY&eqs=U2sDoPeguRL6otaueLU2QuY64svwH8SY%2FtkGZ5krx9FcJUg4DHlADVZpB2z2JgKOVVLsN&ekv=4&page=1&t=1434352126867'
-
-]
-
+#UserAgent集合，变换ua，减少被屏蔽的概率
 user_agents=[
     'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
     'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
@@ -92,13 +70,16 @@ conn= MySQLdb.connect(
         user='wejoy',
         passwd='wejoy',
         db ='wx_reader',
-	charset="utf8"
+	charset='utf8'
 )
+
+# logger = get_logger()
 
 def get_account_page_urls():
     all_urls = []
-    for acccount_url in acccount_urls:
-        acccount_url = acccount_url +  get_current_timestamp()
+    file = open("accounts.txt")
+    for line in file.xreadlines():
+        acccount_url = line +  get_current_timestamp()
         all_urls.append(acccount_url)
     #shuffle
     random.shuffle(all_urls)
@@ -113,6 +94,7 @@ def get_account_data(account_page_url):
         #headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0'}
         headers = {}
         headers['User-Agent'] = random.choice(user_agents)
+        # print headers
         response = requests.get(account_page_url, headers=headers)
         # soup = bs4.BeautifulSoup(response.text)
         req_json = get_regex_value(r_req_data, response.text, 1)
@@ -171,17 +153,20 @@ def parse_args():
     return parser.parse_args()
 
 def start_tasks(options):
-     account_page_urls = get_account_page_urls()
+     # account_page_urls = get_account_page_urls()
     # pool = Pool(options.workers)
     # pool.map(get_account_data, account_page_urls)
     
     #single thread
      while True:
+        account_page_urls = get_account_page_urls()
         for account_page_url in account_page_urls:
             get_account_data(account_page_url)
+            #anti block
             sleep(60 + random.randint(10,300))
         
         #随机一段时间，重新抓取
+        #anti block
         sleep(random.randint(6,15)*60*60)
     
 def get_regex_value(regex, html, index):
@@ -235,7 +220,7 @@ def query_one_data(openid):
     
     return mid
 
-def setLogger():  
+def get_logger():  
     # 创建一个logger,可以考虑如何将它封装  
     logger = logging.getLogger('mylogger')  
     logger.setLevel(logging.DEBUG)  
