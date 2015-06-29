3e6e81f4917ea265c13fa9fa9373ae4f0d286bd7
muzixinly <muzixinly@gmail.com>
2015-06-29 18:44:54 +0800

    add

--- tool/wx_reader.py
+++ tool/wx_reader.py
@@ -85,6 +85,7 @@ def get_account_page_urls():
     for line in file.xreadlines():
         # acccount_url = line +  get_current_timestamp()
         acccount_url = line.strip('\n') 
+        acccount_url = line.strip('\r\n') 
         all_urls.append(acccount_url)
     #shuffle
     random.shuffle(all_urls)
